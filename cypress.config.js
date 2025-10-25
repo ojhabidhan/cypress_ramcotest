c0abf4998adac3187215040b19a5c65a77714d93
ojhabidhan <ojhabidhan@gmail.com>
2025-10-25 22:42:48 +0545

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     // Change this to your app URL if you want baseUrl
-    baseUrl: "https://aimms-sgima.train.tc.canada.ca/extui/hub/",  
+    baseUrl: "https://aimms-sgima.train.tc.canada.ca/extui/",  
     // Where your test files are located
     specPattern: "cypress/e2e/**/*.js",
     // Support file (optional)
