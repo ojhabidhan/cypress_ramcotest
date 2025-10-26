31bf7759173220cd6cd1e3d9df91b27ac991df6d
ojhabidhan <ojhabidhan@gmail.com>
2025-10-26 21:56:15 +0545

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -6,6 +6,7 @@ module.exports = defineConfig({
     defaultCommandTimeout: 60000,        // All cy.get/cy.contains will retry up to 60s
     pageLoadTimeout: 60000,              // cy.visit() will wait up to 60s
     viewportWidth: 1440,
-    viewportHeight: 900
+    viewportHeight: 900,
+    specPattern: 'cypress/e2e/**/*.spec.js' 
   }
 });
